6da21081af4f00c2e396a409722f54ac9de6866a
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-10-31 15:33:42 +0000

    Oct 31, 2024, 11:33 AM

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"60968ee3bde75146dba09d6bb0b7465008890b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4228701892"
+    serviceWorkerVersion: "2503194915"
   }
 });
